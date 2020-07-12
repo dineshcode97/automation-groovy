a1ba2ff6bdd94c9fe1c298326d1f8db13f6fee51
Dinesh <64348165+dineshcode97@users.noreply.github.com>
2020-07-12 14:42:16 +0530

    testing server

--- testserver.sh
+++ testserver.sh
@@ -1,5 +1,11 @@
-ip=$(kubectl get svc -o jsonpath='{.items[*].spec.ports[*].nodePort}')
-echo $ip
+ips=$(kubectl get svc -o jsonpath='{.items[*].spec.ports[*].nodePort}')
+echo $ips
+for ip in $ips
+do
+if test $ip == 31136
+then
+echo ""
+else
 status_code=$(curl -s -o /dev/null -w "%{http_code}" 192.168.99.102:$ip)
 echo $status_code
 if test $status_code == 200
@@ -10,3 +16,5 @@ else
  echo " $status_code ERROR" 
  exit 0
 fi
+fi
+done
