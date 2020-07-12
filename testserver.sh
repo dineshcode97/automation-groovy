ca8780235a80eba908fae702dcab6dc5be74845c
Dinesh <64348165+dineshcode97@users.noreply.github.com>
2020-07-12 14:26:53 +0530

    Update testserver.sh

--- testserver.sh
+++ testserver.sh
@@ -1,15 +1,12 @@
-export PODIP=$(kubectl get svc -o jsonpath='{.items[*].spec.ports[*].nodePort}')
-echo $PODIP
-for podi in $PODIP
-do
-echo $podi
-status_code=$(curl -s -o /dev/null -w "%{http_code}" 192.168.99.102:$podi)
+export pod_ip=$(kubectl get svc -o jsonpath='{.items[*].spec.ports[*].nodePort}')
+echo $pod_ip
+export status_code=$(curl -s -o /dev/null -w "%{http_code}" 192.168.99.100:$pod_ip)
 echo $status_code
-if test $status_code == 200
+if test $status_code -eq 200
 then 
- echo "Working fine"
+ echo "webpage is working properly"
+ exit 1
 else
- echo " $status_code ERROR" 
- echo  "In Pod which nodeport is $podi"
+ echo " $status_code this is the not the webpage you are looking for" 
  exit 0
 fi
