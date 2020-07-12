801f20f9e9bada3f57af4f36af5429660f0f105b
Dinesh <64348165+dineshcode97@users.noreply.github.com>
2020-07-12 15:03:45 +0530

    monitor our servers

--- monitorserver.sh
+++ monitorserver.sh
@@ -6,11 +6,13 @@ then
 if kubectl get deployment | grep webserver
 then
 echo "webserver setup already running"
-else
-kubectl apply -f webserver.yaml
-fi
 POD=$(kubectl get pods -l app=webserver -o jsonpath="{.items[0].metadata.name}")
 kubectl cp *.html $POD:/usr/local/apache2/htdocs/
+else
+kubectl apply -f webserver.yaml
+POD=$(kubectl get pods -l app=webserver -o jsonpath="{.items[0].metadata.name}")
+kubectl cp *.html $POD:/usr/local/apache2/htdocs/
+fi
 else
 echo "No files so no nedd for set up"
 fi
@@ -20,11 +22,13 @@ then
 if  kubectl get deployment | grep php
 then
 echo "php setup already running"
-else
-kubectl apply -f phpserver.yaml
-fi
 POD=$(kubectl get pods -l app=php -o jsonpath="{.items[0].metadata.name}")
 kubectl cp *.php $POD:/var/www/html
+else
+kubectl apply -f phpserver.yaml
+POD=$(kubectl get pods -l app=php -o jsonpath="{.items[0].metadata.name}")
+kubectl cp *.php $POD:/var/www/html
+fi
 else
 echo "No files so no nedd for set up"
 fi
