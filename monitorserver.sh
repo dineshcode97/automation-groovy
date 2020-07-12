9bd541a8ffbc3599fe528c3f44ba7f6306d47b15
Dinesh <64348165+dineshcode97@users.noreply.github.com>
2020-07-12 14:59:28 +0530

    monitor our server

--- monitorserver.sh
+++ monitorserver.sh
@@ -1,7 +1,7 @@
 #htmlfiles= ls /task6data/ | grep .html | wc -l
 #phpfiles=  ls /task6data/ | grep .php | wc -l
 cd /root/task6data/
-if ls * | grep html
+if ls * | grep .html
 then 
 if kubectl get deployment | grep webserver
 then
@@ -9,13 +9,13 @@ echo "webserver setup already running"
 else
 kubectl apply -f webserver.yaml
 fi
+POD=$(kubectl get pods -l app=webserver -o jsonpath="{.items[0].metadata.name}")
+kubectl cp *.html $POD:/usr/local/apache2/htdocs/
 else
 echo "No files so no nedd for set up"
 fi
-POD=$(kubectl get pods -l app=webserver -o jsonpath="{.items[0].metadata.name}")
-kubectl cp *.html $POD:/usr/local/apache2/htdocs/
 
-if ls * | grep php
+if ls * | grep .php
 then 
 if  kubectl get deployment | grep php
 then
@@ -23,8 +23,8 @@ echo "php setup already running"
 else
 kubectl apply -f phpserver.yaml
 fi
+POD=$(kubectl get pods -l app=php -o jsonpath="{.items[0].metadata.name}")
+kubectl cp *.php $POD:/var/www/html
 else
 echo "No files so no nedd for set up"
 fi
-POD=$(kubectl get pods -l app=php -o jsonpath="{.items[0].metadata.name}")
-kubectl cp *.php $POD:/var/www/html
