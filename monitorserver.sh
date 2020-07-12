1290ba88510efc37a784378d36b1a07979262f05
Dinesh <64348165+dineshcode97@users.noreply.github.com>
2020-07-12 10:27:19 +0530

    monitor servers

--- monitorserver.sh
+++ monitorserver.sh
@@ -1,6 +1,6 @@
 htmlfiles= ls /task3data/ | grep .html | wc -l
 phpfiles=  ls /task3data/ | grep .php | wc -l
-cd /task3data/
+cd /task6data/
 if $htmlfiles > 0
 then 
 if kubectl get deployment | grep webserver
@@ -13,7 +13,7 @@ else
 echo "No files so no nedd for set up"
 fi
 POD=$( kubectl get pods -l app=webserver -o jsonpath="{.items[0].metadata.name}")
-kubectl cp /task3data/index.html $POD:/usr/local/apache2/htdocs/
+kubectl cp /task6data/index.html $POD:/usr/local/apache2/htdocs/
 
 if $phpfiles > 0
 then 
@@ -27,4 +27,4 @@ else
 echo "No files so no nedd for set up"
 fi
 POD=$(kubectl get pods -l app=php -o jsonpath="{.items[0].metadata.name}")
-kubectl cp /task3data/index.php $POD:/var/www/html
+kubectl cp /task6data/index.php $POD:/var/www/html
