4f2f047eacb9f65cea81b3deae53598489b5b2ec
Dinesh <64348165+dineshcode97@users.noreply.github.com>
2020-07-12 12:29:49 +0530

    testing server

--- testserver.sh
+++ testserver.sh
@@ -1,4 +1,4 @@
-export PODIP=$(kubectl get svc -o jsonpath='{.items[*].spec.ports[*].nodePort}')
+export PODIP=$(kubectl get svc -o jsonpath='{.items[0].spec.ports[0].nodePort}')
 echo $PODIP
 export status_code=$(curl -s -o /dev/null -w "%{http_code}" 192.168.99.102:$PODIP)
 echo $status_code
